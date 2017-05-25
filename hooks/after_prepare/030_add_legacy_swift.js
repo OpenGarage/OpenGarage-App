c47bdac82e0c710956cf1c1002bbec833b20e63c
Samer Albahra <salbahra@gmail.com>
2017-05-25 11:20:49 -0500

    Remove plugins that do not support Swift 3 (legacy Swift is gone)

--- hooks/after_prepare/030_add_legacy_swift.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-var fs = require( "fs" );
-
-if ( fs.existsSync( "platforms/ios/cordova" ) ) {
-    fs.appendFileSync( "platforms/ios/cordova/build.xcconfig", "\nSWIFT_VERSION=2.3\n" );
-    process.stdout.write( "add legacy swift version" );
-}
