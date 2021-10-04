56d25ae3d3585d6dd839c65a6ebddf91cde79d33
Samer Albahra <salbahra@gmail.com>
2021-10-03 17:31:54 -0700

    Add support for OTC relay
    Based on https://github.com/Derpthemeus/OpenGarage-App

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -156,15 +156,18 @@ angular.module( "opengarage.controllers", [ "opengarage.utils", "opengarage.clou
 				buttons: [
 					{ text: "<i class='icon ion-plus-circled'></i> Add by IP" },
 					{ text: "<i class='icon ion-network'></i> Add by Blynk Token" },
+					{ text: "<i class='icon ion-network'></i> Add by OpenThings Cloud Token" },
 					{ text: "<i class='icon ion-ios-color-wand'></i> Setup New Device" }
 				],
 				titleText: "Add Controller",
 				cancelText: "Cancel",
 				buttonClicked: function( index ) {
-					if ( index === 1 ) {
-						Utils.showAddBlynk();
-					} else if ( index === 0 ) {
+					if ( index === 0 ) {
 						Utils.showAddController();
+					} else if ( index === 1 ) {
+						Utils.showAddBlynk();
+                    } else if ( index === 2 ) {
+                        Utils.showAddOtc();
 					} else {
 						Utils.checkNewController( function( result ) {
 							if ( !result ) {
